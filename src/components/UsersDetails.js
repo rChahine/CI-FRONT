6d437f1d2e6e49f7efd539bf1528720236e6f19a
Lord-Délice <nicolas.asri@epsi.fr>
2020-10-29 11:46:22 +0100

    :construction: working on UsersDetails components

--- src/components/UsersDetails.js
+++ src/components/UsersDetails.js
@@ -11,7 +11,32 @@ class UsersDetails extends React.Component {
   render() {
     return (
       <div>
-        <p>test</p>
+        <h1>Users</h1>
+
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Server</th>
+              <th scope="col">User ID</th>
+              <th scope="col">Username</th>
+              <th scope="col">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">http://mescouilles</th>
+              <td>{this.props.data.account.id}</td>
+              <td>{this.props.data.currentStatus}</td>
+              <td>{this.props.data.currentStatus}</td>
+            </tr>
+            <tr>
+              <th scope="row">http://mescouilles</th>
+              <td>0</td>
+              <td>toto</td>
+              <td>INACTIVE</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
