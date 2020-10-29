6efd52229bec34489012c1476359f6b5042b5871
rChahine <romain.chahine@outlook.fr>
2020-10-29 10:26:26 +0100

    :sparkles: adding app.js componentDidmount

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
+import ChatRoomDetails from './components/ChatRoomDetails';
+import { USERS, CHATROOM } from './mock';
+
 import './App.css';
 
-function App() {
-  return <div className="App">test</div>;
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      chatrooms: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      users: USERS,
+      chatrooms: CHATROOM,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>CI-FRONT</h1>
+        <ChatRoomDetails data={this.state.chatrooms} />
+      </div>
+    );
+  }
 }
 
 export default App;
